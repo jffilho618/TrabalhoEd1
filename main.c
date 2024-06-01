a24cb4c0dead041afa60307cf3ec302e207b418a
joao_batista <jffilho618@gmail.com>
2024-06-01 15:56:47 -0300

    crialçao de fuções transpose rgb e gray

--- main.c
+++ main.c
@@ -27,6 +27,9 @@ int main() {
                 do {
                     printf("[1] Vertical Flip\n");
                     printf("[2] Horizontal Flip\n");
+                    printf("[3] Transpose\n");
+                    printf("[4] Clahe\n");
+                    printf("[5] Median Blur\n");
                     printf("[0] Sair\n");
                     printf("Opcao: ");
                     scanf("%d", &op2);
@@ -40,6 +43,16 @@ int main() {
                         case 2: {
                             img_rgb_copia = flip_horizontal_rgb(img_rgb_copia);
                             print_image_rgb(img_rgb_copia);
+                            break;
+                        }
+                        case 3: {
+                            img_rgb_copia = transpose_rgb(img_rgb_copia);
+                            print_image_rgb(img_rgb_copia);
+                            break;
+                        }
+                        case 5: {
+                            //img_rgb_copia = median_blur_rgb(img_rgb_copia);
+                            //print_image_rgb(img_rgb_copia);
                             break;
                         }
                     }
@@ -56,6 +69,9 @@ int main() {
                 do {
                     printf("[1] Vertical Flip\n");
                     printf("[2] Horizontal Flip\n");
+                    printf("[3] Transpose\n");
+                    printf("[4] Clahe\n");
+                    printf("[5] Median Blur\n");
                     printf("[0] Sair\n");
                     printf("Opcao: ");
                     scanf("%d", &op3);
@@ -69,6 +85,16 @@ int main() {
                         case 2: {
                             img_gray_copia = flip_horizontal_gray(img_gray_copia);
                             print_image_gray(img_gray_copia);
+                            break;
+                        }
+                        case 3: {
+                            img_gray_copia = transpose_gray(img_gray_copia);
+                            print_image_gray(img_gray_copia);
+                            break;
+                        }
+                        case 5: {
+                            //img_gray_copia = median_blur_gray(img_gray_copia);
+                            //print_image_gray(img_gray_copia);
                             break;
                         }
                     }
