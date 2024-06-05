575d50131b85278df4f6c686b011929ade1a1d3f
Jonsrn <jonathansrn2@gmail.com>
2024-06-05 01:35:31 -0300

    Adição do Filtro Mosaico RGB
    Foi adicionado as funções referentes ao Filtro Mosaico Pixel no RGB

--- image.c
+++ image.c
@@ -717,3 +717,137 @@ ImageRGB *Filtro_Sepia(ImageRGB *img_rgb) {
 
     return img_rgb;
 }
+
+//Daqui pra baixo é o filtro mosaico no RGB
+
+
+Bloco_RGB *coletarBlocos_Mosaico_RGB(ImageRGB *img_rgb, int qtdade_blocos, int tamanho_bloco) {
+    Bloco_RGB *blocos = (Bloco_RGB*)malloc(qtdade_blocos * sizeof(Bloco_RGB));
+    if (blocos == NULL) {
+        printf("Falha na alocacao\n");
+        exit(1);
+    }
+
+    int linha = 0;
+    int coluna = 0;
+
+    for (int k = 0; k < qtdade_blocos; k++) {
+        blocos[k].Vetor_Bloco_Blue = (int*) malloc(tamanho_bloco * sizeof(int));
+        blocos[k].Vetor_Bloco_Green = (int*) malloc(tamanho_bloco * sizeof(int));
+        blocos[k].Vetor_Bloco_Red = (int*) malloc(tamanho_bloco * sizeof(int));
+
+        if (blocos[k].Vetor_Bloco_Blue == NULL|| blocos[k].Vetor_Bloco_Red == NULL || blocos[k].Vetor_Bloco_Green == NULL) {
+            printf("Falha na alocacao\n");
+            if(blocos[k].Vetor_Bloco_Blue == NULL){
+                for (int l = 0; l < k; l++) {
+                    free(blocos[l].Vetor_Bloco_Red); 
+                    free(blocos[l].Vetor_Bloco_Blue);
+                    free(blocos[l].Vetor_Bloco_Green);
+                }
+                free(blocos);
+                exit(1); 
+            }
+            if(blocos[k].Vetor_Bloco_Red == NULL){
+                for (int l = 0; l < k; l++) {
+                    free(blocos[l].Vetor_Bloco_Red); 
+                    free(blocos[l].Vetor_Bloco_Blue);
+                    free(blocos[l].Vetor_Bloco_Green);
+                }
+                free(blocos);
+                exit(1); 
+            }
+            if(blocos[k].Vetor_Bloco_Green == NULL){
+                for (int l = 0; l < k; l++) {
+                    free(blocos[l].Vetor_Bloco_Red); 
+                    free(blocos[l].Vetor_Bloco_Blue);
+                    free(blocos[l].Vetor_Bloco_Green);
+                }
+                free(blocos);
+                exit(1); 
+            }
+        }
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                int posicao = (linha + i) * img_rgb->dim.largura + (coluna + j);
+                blocos[k].Vetor_Bloco_Red[i * 5 + j] = img_rgb->pixels[posicao].red;
+                blocos[k].Vetor_Bloco_Green[i * 5 + j] = img_rgb->pixels[posicao].green;
+                blocos[k].Vetor_Bloco_Blue[i * 5 + j] = img_rgb->pixels[posicao].blue;
+            }
+        }
+        blocos[k].posicao = k;
+
+        // Atualiza coluna e linha
+        coluna += 5;
+        if (coluna >= img_rgb->dim.largura) {
+            coluna = 0;
+            linha += 5;
+        }
+    }
+
+    return blocos;
+}
+
+void remontar_Mosaico_RGB(ImageRGB *img_rgb, Bloco_RGB *blocos, int qtdade_blocos) {
+    int linha = 0;
+    int coluna = 0;
+
+    for (int k = 0; k < qtdade_blocos; k++) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                int posicao = (linha + i) * img_rgb->dim.largura + (coluna + j);
+                img_rgb->pixels[posicao].blue = blocos[k].Vetor_Bloco_Blue[i * 5 + j];
+                img_rgb->pixels[posicao].green = blocos[k].Vetor_Bloco_Green[i * 5 + j]; 
+                img_rgb->pixels[posicao].red = blocos[k].Vetor_Bloco_Red[i * 5 + j];
+            }
+        }
+
+        // Atualiza coluna e linha
+        coluna += 5;
+        if (coluna >= img_rgb->dim.largura) {
+            coluna = 0;
+            linha += 5;
+        }
+    }
+}
+
+
+
+ImageRGB *FiltroMosaico_RGB(ImageRGB *img_rgb){
+    Bloco_RGB *blocos; 
+    int qtdade_blocos = 10000;
+    int tamanho_bloco = 25;
+    int media_vermelho, media_azul, media_verde; 
+
+    blocos = coletarBlocos_Mosaico_RGB(img_rgb, qtdade_blocos, tamanho_bloco);
+    media_vermelho = 0; 
+    media_azul = 0; 
+    media_verde = 0; 
+
+    for(int i = 0; i < qtdade_blocos; i++){
+        media_vermelho = 0; 
+        media_azul = 0; 
+        media_verde = 0;
+        for(int j = 0; j < tamanho_bloco; j++){
+          media_azul = blocos[i].Vetor_Bloco_Blue[j] + media_azul; 
+          media_vermelho = blocos[i].Vetor_Bloco_Red[j] + media_vermelho;
+          media_verde = blocos[i].Vetor_Bloco_Green[j] + media_verde;
+        }
+        media_azul = media_azul / tamanho_bloco; 
+        media_verde = media_verde / tamanho_bloco; 
+        media_vermelho = media_vermelho / tamanho_bloco; 
+        for(int k = 0; k < tamanho_bloco; k++){
+            blocos[i].Vetor_Bloco_Blue[k] = media_azul;
+            blocos[i].Vetor_Bloco_Green[k] = media_verde; 
+            blocos[i].Vetor_Bloco_Red[k] = media_vermelho; 
+        }
+
+    }
+
+    remontar_Mosaico_RGB(img_rgb, blocos, qtdade_blocos); 
+
+
+
+    return img_rgb; 
+}
+
